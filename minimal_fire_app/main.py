13fde330c429178f0ec0a0d53c90b2f49f5a9aa5
NVTsirulev <nvtsirulev@mai.education>
2023-03-29 23:08:43 +0300

    add minimal fire app

---
+++ minimal_fire_app/main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from importlib import import_module
+import os
+"""import RPi.GPIO as GPIO"""
+import time
+from flask import Flask, render_template, Response, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        button_value = request.form['button_value']
+        if button_value == 'value':
+            GPIO.setmode(GPIO.BOARD)
+            gpio_value = 1
+            GPIO.setup(gpio_value, GPIO.OUT)
+            GPIO.output(gpio_value, GPIO.HIGH)
+            GPIO.cleanup()
+    else:
+        return render_template('index.html')
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
