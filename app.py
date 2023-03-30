f854b00015f6b7d05f1d2eacaac86410ee9813ed
Ivan Kochkozharov <tegusigaalpa@gmail.com>
2023-03-30 16:26:57 +0300

    some fixes

--- app.py
+++ app.py
@@ -12,15 +12,11 @@ def index():
     if request.method == 'POST':
         button_value = request.form['button_value']
         if button_value == 'value':
-            GPIO.setmode(GPIO.BOARD)
+            GPIO.setmode(GPIO.BCM)
             gpio_value = 15
             GPIO.setup(gpio_value, GPIO.OUT)
             GPIO.output(gpio_value, GPIO.HIGH)
             time.sleep(1)
             GPIO.cleanup()
-    else:
-        return render_template('index.html')
+    return render_template('index.html')
 
-if __name__ == '__main__':
-    app.debug = True
-    app.run()
