7ab7142db37952c095d265f4151bae36c72e973e
Ivan Kochkozharov <tegusigaalpa@gmail.com>
2023-04-03 18:46:44 +0300

    setup script

--- wifi_setup/setup.sh
+++ wifi_setup/setup.sh
@@ -3,8 +3,13 @@ apt install hostapd dnsmasq -y
 systemctl unmask hostapd
 systemctl disable hostapd
 systemctl disable dnsmasq
-
-cp files/autohotspot /usr/bin
-cp files/autohotspot.service files/robotgun.service /etc/systemd/system
-
-#TODO Дописать скрипт
+cp files/hostapd.conf /etc/hostapd/
+cp files/hostapd /etc/default/
+mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
+cp files/dnsmasq.conf /etc/
+cp files/autohotspot /usr/bin/
+cp files/interfaces /etc/network/
+cp files/autohotspot.service files/robotgun.service /etc/systemd/system/
+echo "nohook wpa_supplicant" >> /etc/dhcpcd.conf
+systemctl enable autohotspot.service
+systemctl enable robotgun.serviceh
