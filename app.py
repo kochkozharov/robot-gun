0deb0df989307d1252304887960224300ee47ea3
Ivan Kochkozharov <tegusigaalpa@gmail.com>
2023-04-17 14:42:09 +0300

    test servo

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ from importlib import import_module
 import os
 import RPi.GPIO as GPIO
 import time
+from markupsafe import escape
 from flask import Flask, render_template, Response, request
-
+GPIO.setmode(GPIO.BCM)
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,7 +14,6 @@ def index():
 
 @app.route("/fire")
 def fire():
-    GPIO.setmode(GPIO.BCM)
     gpio_fire = 5
     gpio_signal=6
     GPIO.setup(gpio_signal, GPIO.IN)
@@ -22,5 +22,27 @@ def fire():
     GPIO.output(gpio_fire, GPIO.HIGH)
     while GPIO.input(gpio_signal) == GPIO.HIGH:
         time.sleep(0.01)
+    GPIO.cleanup()
+    return "ok"
+
+
+@app.route('/servo/<int:angle>')
+def servo(angle):
+    gpio_servo=13
+    GPIO.setup(gpio_servo,GPIO.OUT)
+    pwm = GPIO.PWM(gpio_servo,50)
+    pwm.start(0)
+    duty = angle / 18 + 2
+    current_duty = pwm.get_duty_cycle()
+    speed=10
+    if current_duty < duty:
+        for i in range(current_duty, duty, speed):
+            pwm.ChangeDutyCycle(i)
+            time.sleep(0.02)
+    else:
+        for i in range(current_duty, duty, -speed):
+            pwm.ChangeDutyCycle(i)
+            time.sleep(0.02)
+    pwm.stop()
     GPIO.cleanup()
     return "ok"
