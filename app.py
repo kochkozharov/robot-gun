791bf3db9194f67d1e1044e673c6f457bdc53c53
Ivan Kochkozharov <tegusigaalpa@gmail.com>
2023-04-20 00:18:46 +0300

    second servo (needs to be tested...)

--- app.py
+++ app.py
@@ -7,6 +7,14 @@ from markupsafe import escape
 from flask import Flask, render_template, Response, request
 app = Flask(__name__)
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(12, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
+pwm1 = GPIO.PWM(12, 100)
+pwm2 = GPIO.PWM(13, 100)
+pwm1.start(5)
+pwm2.start(5)
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -23,14 +31,16 @@ def fire():
         time.sleep(0.01)
     return "ok"
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(13, GPIO.OUT)
-pwm = GPIO.PWM(13, 100)
-pwm.start(5)
-
-@app.route('/servo/<int:angle>')
-def servo(angle):
+@app.route('/servo1/<int:angle>')
+def servo1(angle):
     duty = float(angle) / 2.5 + 2.5
-    global pwm
-    pwm.ChangeDutyCycle(duty)
+    global pwm1
+    pwm1.ChangeDutyCycle(duty)
+    return "ok"
+
+@app.route('/servo2/<int:angle>')
+def servo2(angle):
+    duty = float(angle) / 2.5 + 2.5
+    global pwm2
+    pwm2.ChangeDutyCycle(duty)
     return "ok"
